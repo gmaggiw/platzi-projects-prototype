4b2feaa07e13605ad9c7a251d3f6d7bfd9fc5b91
Gustavo Maggi <gmaggiw@gmail.com>
2018-09-17 07:51:59 -0500

    Adding Projects App

--- platziprojects/platziprojects/settings.py
+++ platziprojects/platziprojects/settings.py
@@ -36,9 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'material',
-    'material.frontend',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
