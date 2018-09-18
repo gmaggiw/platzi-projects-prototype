8f559da2e8278b5973ad710a7259fa7c84797c39
Gustavo Maggi <gmaggiw@gmail.com>
2018-09-18 08:53:13 -0500

    Adding stars management

--- platziprojects/projects/models.py
+++ platziprojects/projects/models.py
@@ -15,9 +15,16 @@ class Project(models.Model):
     github = models.URLField(max_length=255)
     logo = models.ImageField(upload_to='media/logos', default = 'media/logos/nologo.png')
     stacks = models.ManyToManyField(Stack)
+    stars = models.IntegerField(default = 0)
 
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    def save(self, *args, **kwargs):
+        if self.stars > 5 or self.stars < 0:
+            return 'Error: Stars have to be less or equal to 5'
+        else:
+            super().save(*args, **kwargs)
 
 
 
