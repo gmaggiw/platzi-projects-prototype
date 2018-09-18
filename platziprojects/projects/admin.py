adca95842eec7020b4a0ca0d80a184ca128fe676
Gustavo Maggi <gmaggiw@gmail.com>
2018-09-18 11:08:48 -0500

    Adding Stack admin class

--- platziprojects/projects/admin.py
+++ platziprojects/projects/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from projects.models import Project, Stack
+from django.utils.html import mark_safe
 # Register your models here.
 
-admin.site.register(Project)
-admin.site.register(Stack)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = ('pk','name','description','members','github','logo_image')
+    list_display_links = ('pk','name')
+    search_fields = ('name','description','github')
+
+    list_filter = ('created_at','updated_at')
+
+    def logo_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" />'.format(
+            url = obj.logo.url,
+            width=72
+            )
+    )
+
+@admin.register(Stack)
+class StackAdmin(admin.ModelAdmin):
+    list_display = ('pk','name','description')
+    list_display_links = ('pk','name')
+    search_fields = ('name','description')
